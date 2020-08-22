b59c3a925ba3dce52e0951396e60e5fd8d256143
Bruno <brunochirelli@gmail.com>
2020-08-22 12:32:16 -0300

    add material-ui and gatsby plugins related to it

--- gatsby-config.js
+++ gatsby-config.js
@@ -27,6 +27,15 @@ module.exports = {
         icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
       },
     },
+    `gatsby-theme-material-ui`,
+    {
+      resolve: `gatsby-plugin-material-ui`,
+      options: {
+        stylesProvider: {
+          injectFirst: true,
+        },
+      },
+    },
     `gatsby-plugin-styled-components`,
   ],
-}
+};
