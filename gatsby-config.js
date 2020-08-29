1837b973307e60ba83a7eedf899c71174a68fbd1
Bruno <brunochirelli@gmail.com>
2020-08-29 18:22:16 -0300

    update gatsby config data

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,8 +1,8 @@
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Default Starter`,
-    description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
-    author: `@gatsbyjs`,
+    title: `Bruno Chirelli Resume`,
+    description: `Hi! I'm Bruno Chirelli, a brazillian React frontend developer.`,
+    author: `@brunochirelli`,
   },
   plugins: [
     `gatsby-plugin-react-helmet`,
@@ -34,7 +34,7 @@ module.exports = {
         background_color: `#663399`,
         theme_color: `#663399`,
         display: `minimal-ui`,
-        icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
+        icon: `src/images/bruno_square.jpg`, // This path is relative to the root of the site.
       },
     },
     `gatsby-theme-material-ui`,
